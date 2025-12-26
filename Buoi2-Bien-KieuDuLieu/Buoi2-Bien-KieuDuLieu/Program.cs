edec29f0a268b91c620cf6c426fe167359dfb390
xhuy2608 <xuanhuy260803@gmail.com>
2025-12-26 23:52:40 +0700

    buoi2-bienvakieudulieu

---
+++ Buoi2-Bien-KieuDuLieu/Buoi2-Bien-KieuDuLieu/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Buoi2_Bien_KieuDuLieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //1. Biến số
+            //int count;
+            //int maxValue;
+            //2.Biến chuỗi
+            //string fullName;
+            //3.Biến mảng/ dan h sách
+            //int[] scores;
+            //List<string> scoreList;
+
+            //4. Biến hằng số (constant)
+            //const int TotalScore = 100;
+
+            //5. Khởi tạo biến - gán lại giá trị cho biến
+            //Khai báo biến lúc biên dịch
+            //int x = 5;
+            //x = 10;
+            //Console.WriteLine("Gia tri x:"+x);
+
+            //Khai báo biến lúc chạy chương trình
+            Console.Write("Nhap dia chi nha Huy:");
+            string addressv2=Console.ReadLine();
+            Console.WriteLine("Dia chi nha Huy:"+addressv2);
+
+
+         
+
+        }
+    }
+}
---
+++ Buoi2-Bien-KieuDuLieu/Buoi2-KieuDuLieu/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Buoi2_KieuDuLieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* //Khai báo và khởi tạo các kiểu dữ liệu
+            char a= 'H';
+            int i = 123;
+            short s = 56;
+            long l = 3443;
+            uint ui = 945;
+            ushort us = 716;
+            ulong ul = 3624573;
+            double d = 8.358674532;
+            float f = 3.7330645f;
+            decimal dec = 389.5m;
+
+            //In ket qua
+            Console.WriteLine("char: "+a);
+            Console.WriteLine("integer: "+i);
+            Console.WriteLine("short: "+s);
+            Console.WriteLine("long: "+l);
+            Console.WriteLine("double: "+d);
+            Console.WriteLine("float: "+f);
+            Console.WriteLine("decimal: "+dec);
+            Console.WriteLine("unsigned integer: " + ui);
+            Console.WriteLine("unsigned short: " + us);
+            Console.WriteLine("unsigned long: " + ul);
+            */
+
+
+            unsafe
+            {
+                // Khai báo biến 
+                int n = 10;
+                // Lưu địa chỉ của biến n vào biến con trỏ p 
+                int* p = &n;
+                Console.WriteLine("Value: {0}", n);
+                Console.WriteLine("Address: {0}", (int)p);
+            }
+
+        }
+    }
+}
